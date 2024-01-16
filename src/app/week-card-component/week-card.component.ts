b01ae739ffaada889d07f6e7cb7d7bb2e41757a8
Manuel26190 <manuelfaure26190@gmail.com>
2024-01-16 14:28:33 +0100

    save

--- src/app/week-card-component/week-card.component.ts
+++ src/app/week-card-component/week-card.component.ts
@@ -13,20 +13,10 @@ export class WeekCardComponent {
   weekDayData: WeekDaySchema[] = weekDayData;
   familyData: FamilySchema[] = familyData;
 
-  selectedPersonMatin: string = ''; // Déclarez la propriété ici et initialisez-la si nécessaire
-  selectedPersonMidi: string = ''; // Ajoutez les autres propriétés si nécessaire
-  selectedPersonSoir: string = '';
-
   getPersonPicture(personFirstName: string): string {
-    // Ici, vous devriez mettre en place la logique pour récupérer le chemin de l'image
-    // correspondant à la personne dont le prénom est personFirstName.
-    // Vous pouvez, par exemple, parcourir le tableau familyData pour trouver la personne
-    // et retourner le chemin de son image.
-
-    // Exemple de logique (assurez-vous d'ajuster cela en fonction de votre structure de données réelle) :
     const selectedPerson = this.familyData.find(
       (person) => person.firstname === personFirstName
     );
-    return selectedPerson ? selectedPerson.picture : ''; // Retourne le chemin de l'image ou une chaîne vide si la personne n'est pas trouvée.
+    return selectedPerson ? selectedPerson.picture : '';
   }
 }
